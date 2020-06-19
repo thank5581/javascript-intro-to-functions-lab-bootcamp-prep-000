2c048c6fff9eec34b1e120be4b214b28eb1da764
thank5581 <thank5581@gmail.com>
2020-06-19 17:57:32 -0400

    say hi to gramma

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout (string) {
+    return string.toUpperCase()
+}
+function whisper(string) {
+    return string.toLowerCase()
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+funtion logWhisper(string) {
+    console.log(string.toUpperCase())
+}
+
+function sayHiToGramma(string) {
+    var newstring = string;
+    if (newstring.toLowCase() === string)
+        return "I can't hear you!"
+}
+else if (newstring.toUpperCase() === string){
+    return "YES INDEED!";
+}
+else {
+    return "I love you, too.";
+}
+}
